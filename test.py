2d723fe03e06e2bea4010e01e80ed7219a02fc43
unicycledumptruck <junk@jeffhighsmith.com>
2021-12-06 17:00:50 -0500

    Testing read functions, can't read user or TID mem

--- test.py
+++ test.py
@@ -3,13 +3,12 @@ from __future__ import print_function
 import time
 from datetime import datetime
 import mercury
-reader = mercury.Reader("llrp://izar-51e4c8.local")
+reader = mercury.Reader("llrp://izar-51e4c8.local", protocol="GEN2")
 
 print(reader.get_model())
 print(reader.get_supported_regions())
 
-#reader.set_region("EU3")
-reader.set_read_plan([1], "GEN2", read_power=1900)
+reader.set_read_plan([1], "GEN2", read_power=1900) # Adding bank=["user"], causes segmentation fault
 print(reader.read())
 
 reader.start_reading(lambda tag: print(tag.epc, tag.antenna, tag.read_count, tag.rssi, datetime.fromtimestamp(tag.timestamp)))
