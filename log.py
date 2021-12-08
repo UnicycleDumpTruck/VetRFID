206ec3c8ab287c5486da2ccdcfcb6599dfe59f54
unicycledumptruck <junk@jeffhighsmith.com>
2021-12-08 17:38:41 -0500

    starting to add species lookup

--- log.py
+++ log.py
@@ -10,41 +10,21 @@ log_format = {
 }
 
 
+
 def epc_serial(epc):
     # Return tag serial number portion of EPC code
     return epc[0:12]
 
+def epc_to_string(bepc):
+    return str(bepc)[2:26]
 
 def epc_species_num(epc):
-    return epc[13:16]
-
-
-def epc_to_string(bepc):
-    return str(epcb)[2:26]
+    return epc[12:16]
 
 
 def epc_to_bytes(sepc):
     return bytes(sepc, encoding="UTF8")
 
-
-def old_log_tag(tag):
-    current_time = datetime.now()
-    print("Read: ", tag.epc, tag.antenna, tag.read_count, tag.rssi)
-    print("Log before reading: ")
-    df = pd.read_csv('tag_log.csv')
-    # df.set_index('epc')
-    print(df.to_string())
-    if not (df.loc[df['epc'] == str(tag.epc)].empty):
-        print("Row found")
-        df.loc[df['epc'] == str(tag.epc), 'last_seen'] = current_time
-        #previous_num_reads = df.loc[df['epc'] == str(tag.epc)][0]['num_reads']
-        #print("num_reads", previous_num_reads)
-        #df.loc[df['epc'] == str(tag.epc), 'last_seen'] = current_time
-    else:
-        df.loc[len(df.index)] = [tag.epc, tag.epc[12:16],
-                                 current_time, current_time, 0]
-    print("Dataframe after:\n", df.to_string())
-    df.to_csv('tag_log.csv', index=False)  # , index=False
 
 def json_import(filename):
     with open(filename, 'r') as f:
@@ -57,14 +37,37 @@ def json_export(filename, ldict):
         jstr = json.dumps(ldict)
         f.write(jstr)
 
+def print_log_dict(ldict):
+    for itm in ldict.items():
+        print(itm[0], itm[1].values())
 
 
-epcb = bytes("000000000001000120211207", encoding="UTF8")
+def log_epc(epc): # string input
+    log_dict = json_import('jlog.txt')
+    if log_dict.get(epc):
+        #print(epc)
+        log_dict[epc]['last_seen'] = str(datetime.now())
+        log_dict[epc]['num_reads'] = str(int(log_dict[epc]['num_reads']) + 1)
+    else:
+        log_dict[epc] =  { 'species':epc_species_str(epc), 
+                                'first_seen':str(datetime.now()),
+                                'last_seen':str(datetime.now()),
+                                'num_reads':'1',
+                                }
+    #log_dict['100000000000000000000000']['species'] = "cat"
+    json_export('jlog.txt', log_dict)
 
-#log_epc(epcb)
+def log_tag(tag):
+    string_epc = epc_to_string(tag.epc)
+    log_epc(string_epc)
+    print("Logged: ", string_epc)
 
-log_dict = json_import('jlog.txt')
-for itm in log_dict.items():
-    print(itm)
-log_dict['100000000000000000000000']['species'] = "cat"
-json_export('jlog.txt', log_dict)
+# log_epc("000000000001000120211207")
+# log_epc("000000000001000120211208")
+# log_epc("000000000001000120211209")
+
+species_dict = json_import('species.json')
+
+
+def epc_species_str(epc):
+    return species_dict[epc_species_num(epc)]
