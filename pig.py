f52958ae333c68ba439b199c04e29850e3381d64
unicycledumptruck <junk@jeffhighsmith.com>
2021-12-09 19:28:50 -0500

    trying event dispatchers

--- pig.py
+++ pig.py
@@ -1,5 +1,25 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import time
+from datetime import datetime
+import mercury
+import log
 import pyglet
 import os
+from pyglet.event import EventDispatcher
+
+
+#reader = mercury.Reader("llrp://izar-51e4c8.local", protocol="GEN2")
+reader = mercury.Reader("tmr:///com2", protocol="GEN2")
+
+print(reader.get_model())
+print(reader.get_supported_regions())
+
+reader.set_read_plan([1,2], "GEN2", read_power=1900) # Adding bank= causes segmentation fault, maybe tags don't support
+#print(reader.read())
+
+
+
 os.environ['DISPLAY'] = ':1'
 window = pyglet.window.Window()
 image = pyglet.resource.image('media/dog/xray/001.png')
@@ -10,10 +30,34 @@ label = pyglet.text.Label('Neigh/Woof/Meow',
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
 
+class TagDispatcher(EventDispatcher):
+    def tag_read(self, epc):
+        self.dispatch_event('on_tag_read', epc.epc)
+    def on_tag_read(self, epc):
+        print("TagDispatcher Dispatched: ", epc.epc)
+
+
+TagDispatcher.register_event_type('on_tag_read')
+td = TagDispatcher()
+
 @window.event
 def on_draw():
     window.clear()
     image.blit(0,0)
     label.draw()
 
+@window.event
+def on_tag_read(epc):
+    print("Window rx epc: ", epc)
+    label = pyglet.text.Label(epc.epc,
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+#reader.start_reading(log.log_tag)
+reader.start_reading(td.tag_read)
+
 pyglet.app.run()
+
+reader.stop_reading()
