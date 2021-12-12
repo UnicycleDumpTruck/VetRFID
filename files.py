73c97458549c4a26ce2eb75eaa8869eda26dea6e
Jeff <junk@jeffhighsmith.com>
2021-12-12 17:31:20 -0500

    images displaying when appropriate tag scanned!

--- files.py
+++ files.py
@@ -1,4 +1,8 @@
 import json
+import species
+import pyglet
+import os
+import random
 
 def json_import(filename):
     with open(filename, 'r') as f:
@@ -11,3 +15,11 @@ def json_export(filename, ldict):
     with open(filename, 'w') as f:
         jstr = json.dumps(ldict)
         f.write(jstr)
+
+file_types = {'img': pyglet.resource.image,}
+
+def random_species_dir_type(animal_species, media_directory, media_type): 
+    # animal_species="monkey", media_directory="xray"
+    p = "media/" + animal_species + "/" + media_directory + "/"
+    f = file_types[media_type](p + random.choice(os.listdir(p)))
+    return f
