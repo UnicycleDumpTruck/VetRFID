ed9ad3dd119cf17340868f3eb439f6e2f5a68ea4
UnicycleDumpTruck <junk@jeffhighsmith.com>
2021-12-15 22:29:39 -0500

    added docstrings and type hints

--- log.py
+++ log.py
@@ -11,28 +11,29 @@ import species
 
 
 def print_log_dict(ldict):
+    """Print dictionary line by line."""
     for itm in ldict.items():
         print(itm[0], itm[1].values())
 
 
-def log_epc(epc): # string input
+def log_epc(epc):  # string input
+    """Log epc string to jlog.json file."""
     log_dict = files.json_import('jlog.json')
     if log_dict.get(epc):
-        #print(epc)
+        # print(epc)
         log_dict[epc]['last_seen'] = str(datetime.now())
         log_dict[epc]['num_reads'] = str(int(log_dict[epc]['num_reads']) + 1)
     else:
-        log_dict[epc] =  { 'species':species.species_str(epc.epc_species_str(epc)), 
-                                'first_seen':str(datetime.now()),
-                                'last_seen':str(datetime.now()),
-                                'num_reads':'1',
-                                }
+        log_dict[epc] = {'species': species.species_str(epc.epc_species_str(epc)),
+                         'first_seen': str(datetime.now()),
+                         'last_seen': str(datetime.now()),
+                         'num_reads': '1',
+                         }
     files.json_export('jlog.json', log_dict)
 
 
 def log_tag(tag):
+    """Get string of epc from tag and send it to log_epc function for jlog.json."""
     string_epc = epc.epc_to_string(tag.epc)
     log_epc(string_epc)
     print("Logged: ", string_epc)
-
-
