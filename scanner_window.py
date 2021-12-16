6671e21a632cca4a980ffade4614583935710bf9
UnicycleDumpTruck <junk@jeffhighsmith.com>
2021-12-15 20:25:58 -0500

    split scanner_window & tag_dispatcher into separate files

---
+++ scanner_window.py
@@ -0,0 +1,78 @@
+import pyglet
+import files
+import epc
+import species
+import log
+
+
+class ScannerWindow(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.label_batch = pyglet.graphics.Batch()
+        self.label = pyglet.text.Label('Please place the patient in the scanning area.',
+                                       color=(255, 255, 255, 255),
+                                       font_size=24,
+                                       x=self.width // 2, y=self.height // 2,
+                                       anchor_x='center', anchor_y='center')
+        self.image = None
+        self.clock = pyglet.clock.get_default()
+
+    def on_tag_read(self, tag):
+        self.clear()
+        # self.image = cat
+        tag_string = epc.epc_to_string(tag)
+        spec = species.species_str(epc.epc_species_num(tag_string)).lower()
+        media_dir = 'xray'
+        media_type = 'img'
+        self.image = files.random_species_dir_type(spec, media_dir, media_type)
+        # print("Window Class rx epc: ", tag_string)
+        # clock.schedule_once(self.label_change, 0, tag) workaround before label change worked
+        self.label = pyglet.text.Label(text=spec.capitalize(),
+                                       color=(255, 0, 0, 255),
+                                       font_size=64,
+                                       x=self.width // 2, y=self.height // 2,
+                                       anchor_x='center', anchor_y='center')
+        self.label.draw()
+
+        self.flip()  # Required to cause window refresh
+        return pyglet.event.EVENT_HANDLED
+
+    def on_key_press(self, symbol, modifiers):
+        self.clear()
+        self.flip()
+
+    def on_key_release(self, symbol, modifiers):
+        print("Window RX Keypress")
+        self.clear()
+        self.image = None
+        self.label = pyglet.text.Label("Keys, keys, keys!",
+                                       color=(255, 0, 0, 255),
+                                       x=self.width // 2, y=self.height // 2,
+                                       font_size=12)
+        self.label.draw()
+        self.clock.schedule_once(self.idle, 2)
+
+    def idle(self, dt):
+        self.clock.unschedule(self.idle)
+        print("Going idle, ", dt, " seconds since scan.")
+        self.clear()  # why was this commented out?
+        self.image = None
+        self.label = pyglet.text.Label('Please place the patient in the scanning area.',
+                                       color=(255, 255, 255, 255),
+                                       font_size=24,
+                                       x=self.width // 2, y=self.height // 2,
+                                       anchor_x='center', anchor_y='center')
+        self.label.draw()
+
+    def on_draw(self):
+        self.clear()
+        if self.image:
+            self.image.blit(0, 0)
+        if self.label:
+            self.label.draw()
+        # self.label_batch.draw()
+        # print("on draw label text: ", self.label.text)
+        # self.flip()
+
+
+ScannerWindow.register_event_type('on_tag_read')
---
+++ tag_dispatcher.py
@@ -0,0 +1,43 @@
+import pyglet
+import epc
+
+
+class TagDispatcher(pyglet.event.EventDispatcher):
+    def __init__(self, reader, window1, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.window1 = window1
+        self.reader = reader
+        self.clock = pyglet.clock.get_default()
+
+    def read_tags(self, dt):
+        tag_list = self.reader.read()
+        if tag_list:
+            self.clock.unschedule(self.window1.idle)
+            for tag in tag_list:
+                print("Read EPC: ", epc.epc_to_string(
+                    tag), ", RSSI: ", tag.rssi)
+            tag_list.sort(key=lambda tag: tag.rssi)
+            best_tag = tag_list[0]
+            # TODO log.log_tag(best_tag)
+            best_tag_string = epc.epc_to_string(best_tag)
+            print("Highest signal from read: ", best_tag_string)
+            self.window1.dispatch_event('on_tag_read', best_tag)
+            print("Dispacted tag: ", best_tag_string)
+            # TODO send tag to correct monitors
+        else:
+            # TODO Idle monitors of empty antennas.
+
+            self.clock.schedule_once(self.window1.idle, 1)
+
+    def tag_read(self, tag):
+        epc_string = epc.epc_to_string(tag)
+        # print("Tag read: ", epc_string)
+        self.window1.dispatch_event('on_tag_read', epc_string)
+
+    def on_tag_read(self, epc):
+        # I don't think we need this function
+        # print("TagDispatcher Dispatched: ", epc)
+        pass
+
+
+TagDispatcher.register_event_type('on_tag_read')
