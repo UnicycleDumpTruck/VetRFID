3272f7beaa648d704d09e905cfd58117692a134c
UnicycleDumpTruck <junk@jeffhighsmith.com>
2021-12-18 15:51:13 -0500

    put labels in batch and list. Added last_seen label

--- epc.py
+++ epc.py
@@ -9,6 +9,7 @@ class Tag():
         self.rssi = None
         self.phase = None
         self.read_count = None
+        self.last_seen = None
 
     def serial(self):
         """Return tag serial number portion of EPC code."""
@@ -19,10 +20,11 @@ class Tag():
         return self.epc[12:16]
 
     def species_string(self):
+        """Return species name as a string, like 'horse'."""
         return species.species_str(self.species_num()).lower()
 
     def epc_bytes(self):
-        """Convert EPC string to bytes object."""
+        """Return epc as a bytes object."""
         return bytes(self.epc, encoding="UTF8")
 
     def __repr__(self):
@@ -30,19 +32,24 @@ class Tag():
 
 
 class rTag(Tag):
+    """Class for tags from reader hardware."""
+
     def __init__(self, tag):
         self.epc = str(tag.epc)
         self.phase = tag.phase
         self.antenna = tag.antenna
         self.read_count = tag.read_count
         self.rssi = tag.rssi
-# TODO add methods below to rTag class
+        self.last_seen = None
 
 
 class fTag(Tag):
+    """Class for fake/mock tags for testing w/o hardware."""
+
     def __init__(self, epc, antenna, rssi, phase, read_count):
         self.epc = epc
         self.antenna = antenna
         self.rssi = rssi
         self.phase = phase
         self.read_count = read_count
+        self.last_seen = None
