a0c47dede29135f14ecf9f9f78fe9ac359078ad1
UnicycleDumpTruck <junk@jeffhighsmith.com>
2021-12-18 12:06:43 -0500

    moved labels

--- scanner_window.py
+++ scanner_window.py
@@ -19,11 +19,11 @@ class ScannerWindow(pyglet.window.Window):
                                        font_size=24, font_name='Gilroy',
                                        x=self.width // 2, y=self.height // 2,
                                        anchor_x='center', anchor_y='center')
-        self.window_label = pyglet.text.Label(f"Station #{str(self.window_number)}",
-                                              color=(255, 255, 255, 255),
-                                              font_size=24, font_name='Gilroy',
-                                              x=5, y=5,
-                                              anchor_x='left', anchor_y='bottom')
+        self.station_label = pyglet.text.Label(f"Station #{str(self.window_number)}",
+                                               color=(255, 255, 255, 255),
+                                               font_size=24, font_name='Gilroy',
+                                               x=5, y=self.height - 5,
+                                               anchor_x='left', anchor_y='top')
         self.image = None
         self.clock = pyglet.clock.get_default()
 
@@ -37,11 +37,11 @@ class ScannerWindow(pyglet.window.Window):
             self.species = spec
             self.image = files.random_species_dir_type(
                 spec, self.media_dir, self.media_type)
-            self.label = pyglet.text.Label(text=spec.capitalize(),
+            self.label = pyglet.text.Label(text=f'Species detected: {spec.capitalize()}',
                                            color=(255, 0, 0, 255),
-                                           font_size=64, font_name='Gilroy',
-                                           x=self.width // 2, y=self.height // 2,
-                                           anchor_x='center', anchor_y='center')
+                                           font_size=32, font_name='Gilroy',
+                                           x=self.width - 5, y=5,
+                                           anchor_x='right', anchor_y='bottom')
             self.label.draw()
             self.flip()  # Required to cause window refresh
             self.clock.schedule_once(self.idle, 1)
@@ -82,7 +82,7 @@ class ScannerWindow(pyglet.window.Window):
             self.image.blit(0, 0)
         if self.label:
             self.label.draw()
-        self.window_label.draw()
+        self.station_label.draw()
         # self.label_batch.draw()
         # print("on draw label text: ", self.label.text)
         # self.flip()
