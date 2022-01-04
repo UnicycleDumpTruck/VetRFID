04d4585d25951a5ffef81f9d9a214d69255752c6
UnicycleDumpTruck <junk@jeffhighsmith.com>
2022-01-03 20:01:19 -0500

    tweaked file load for tests

--- tests/files/test_all_files_loading.py
+++ tests/files/test_all_files_loading.py
@@ -1,12 +1,13 @@
 """Test loading of files."""
-import os
+from os import path, listdir
 # import pytest
 import pyglet  # type: ignore
 
 # working_dir = os.path.dirname(os.path.realpath(__file__))
-MEDIA_DIR = os.path.join('media')
-all_species_dirs = [os.path.join(MEDIA_DIR, dirname, "xray/")
-                    for dirname in os.listdir(MEDIA_DIR)
+two_up = path.abspath(path.join(__file__, "../../.."))
+MEDIA_DIR = path.join(two_up, 'media')
+all_species_dirs = [path.join(MEDIA_DIR, dirname, "xray/")
+                    for dirname in listdir(MEDIA_DIR)
                     if dirname[0] != "."]
 print(all_species_dirs)
 
@@ -14,15 +15,20 @@ print(all_species_dirs)
 # pyglet.resource.path = []
 # pyglet.resource.reindex()
 
+def test_load_one_xray():
+    img = pyglet.image.load(path.abspath(
+        path.join(MEDIA_DIR, 'cat/xray/001.jpg')))
+    assert isinstance(img, pyglet.image.ImageData)
+
 
 def test_load_every_xray():
     """Test load every xray image."""
     for species_dir in all_species_dirs:
         files = [f"{species_dir}{filename}"
-                 for filename in os.listdir(species_dir)
+                 for filename in listdir(species_dir)
                  if filename[0] != "."]
         print(files)
         for file in files:
-            img = pyglet.image.load(os.path.abspath(file))
+            img = pyglet.image.load(path.abspath(file))
             print(f"Loaded {type(img)} of size {img.width}, {img.height}")
             assert isinstance(img, pyglet.image.ImageData)
